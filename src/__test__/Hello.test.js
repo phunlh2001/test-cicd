681b53670c1bb52f82f40feba7ac445e0b184fd8
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2024-02-21 01:04:21 +0700

    add teardown function

--- src/__test__/Hello.test.js
+++ src/__test__/Hello.test.js
@@ -1,5 +1,9 @@
-import { render, screen } from '@testing-library/react'
+import { cleanup, render, screen } from '@testing-library/react'
 import Hello from '../components/Hello'
+
+afterAll(() => {
+  cleanup()
+})
 
 test('render hello component', () => {
   render(<Hello />)
